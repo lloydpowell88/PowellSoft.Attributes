6ed407284dac842771a82bfa8144ddf33bb76912
lloyd.powell <lloyd.powell@RST412>
2019-01-15 08:15:49 +0000

    Initial create

---
+++ src/PowellSoft.Attributes/ValidateModelStateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace PowellSoft.Attributes
+{
+    public class ValidateModelStateAsyncAttribute : IAsyncActionFilter
+    {
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
